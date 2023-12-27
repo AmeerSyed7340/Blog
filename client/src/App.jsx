de1ee7452381b110cb46a938a73bafebf6c91765
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-12-27 17:26:31 -0500

    Added a navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button';
+import NavBar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button variant="outlined"> Button MUI</Button >
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
     </>
   )
 }
