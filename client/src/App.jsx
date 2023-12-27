acf7a45a4875ce3e45d60f6eaa45d0a2d2fd8779
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-12-27 18:28:45 -0500

    created pages and set routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./views/Homepage";
+import ReadPage from "./views/Readpage";
+import WritePage from "./views/Writepage";
 import NavBar from './components/Navbar';
+
 
 function App() {
   return (
     <>
-      <NavBar />
+    <NavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog/:id" element={<ReadPage />} />
+          <Route path="/write" element={<WritePage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/views/Readpage.jsx
@@ -0,0 +1,7 @@
+export default function ReadPage(){
+    return(
+        <>
+        <p>Reading page</p>
+        </>
+    )
+}
