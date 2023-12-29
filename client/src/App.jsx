46638dabc38caf86e6fcf4acfbc2004a817833b6
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-12-29 06:50:34 -0500

    Fixed parser issue with AuthContext file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,34 +4,36 @@ import HomePage from "./views/Homepage";
 import ReadPage from "./views/Readpage";
 import WritePage from "./views/Writepage";
 import NavBar from './components/Navbar';
-
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-  const [authorize_flag, setAuthorize_flag] = useState(false); 
+  const [authorize_flag, setAuthorize_flag] = useState(false);
   const [displayName, setDisplayName] = useState("");
 
-  function displayHandler(value){
+  function displayHandler(value) {
     setDisplayName(value);
   }
 
   return (
-    <>
-    <NavBar
-      displayName = {displayName}
-      setDisplayName = {displayHandler}
-      authorize_flag = {authorize_flag}
-      setAuthorize_flag = {setAuthorize_flag}
-    />
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/blog/:id" element={<ReadPage />} />
-        <Route path="/write" element={
-            authorize_flag ? <WritePage /> : <Navigate to="/" />
-          } />
-      </Routes>
-    </Router>
-    </>
+    <AuthProvider>
+      <>
+        <NavBar
+          displayName={displayName}
+          setDisplayName={displayHandler}
+          authorize_flag={authorize_flag}
+          setAuthorize_flag={setAuthorize_flag}
+        />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/blog/:id" element={<ReadPage />} />
+            <Route path="/write" element={
+              authorize_flag ? <WritePage /> : <Navigate to="/" />
+            } />
+          </Routes>
+        </Router>
+      </>
+    </AuthProvider>
   );
 }
 
