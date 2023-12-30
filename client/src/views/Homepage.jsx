1a9914fe8b1a3d40407dd1354dd6d3aeb30e6a69
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-12-29 21:30:12 -0500

    updated homepage with component

--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -1,7 +1,59 @@
-export default function HomePage(){
-    return(
-        <>
-        <p>Home page</p>
-        </>
+import { useEffect, useState } from "react"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+export default function HomePage() {
+    const [blogs, setBlogs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    //useeffect to mount right at the beginning.
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:3000/api/blogs/read");
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                console.log(data.blog);
+                setBlogs(data.blog);
+            }
+            catch (e) {
+                console.error('Error fetching data:', error);
+                setError(error.message);
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div>
+            {blogs.map(blog => {
+                <Card key={blog._id} sx={{ marginBottom: 2 }}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {blog.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {blog.content}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            })}
+        </div>
     )
 }
