31070fa8e9c00e8066becd88e04a4aac4b51c4d8
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-12-30 18:23:51 -0500

    Set up useLocation to get object_id for blog

--- client/src/views/Readpage.jsx
+++ client/src/views/Readpage.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
+
 export default function ReadPage(){
+    const location = useLocation();
+    const pathSegments = location.pathname.split('/');
+    const secondSegment = pathSegments[2] || 'Default Value';
+    console.log(secondSegment);
+
     return(
         <>
         <p>Reading page</p>
