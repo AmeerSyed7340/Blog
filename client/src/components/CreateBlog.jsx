4b0b8d3e2d8fbdb5f162283eb8c6d20ab9015662
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-12-31 08:56:13 -0500

    Added working Guest login btn which allows guest users to write a blog

--- client/src/components/CreateBlog.jsx
+++ client/src/components/CreateBlog.jsx
@@ -1,63 +1,16 @@
 import Button from '@mui/material/Button';
-import { useAuth } from '../contexts/AuthContext';
-import { useState } from 'react';
-import CreateBlogDialog from './CreateBlogDialog';
 import { useNavigate } from 'react-router-dom';
 
-export default function CreateBlog({}) {
-    const { username } = useAuth();
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+export default function CreateBlog() {
 
     const navigate = useNavigate(); // Use the hook directly
 
     const goToWritePage = () => {
         navigate('/write');
-    }
-    const handleClickOpen = () => {
-        setDialogOpen(true);
-    };
-
-    const handleClose = () => {
-        setDialogOpen(false);
-    };
-
-    async function endpoint_call() {
-        //console.log(Username, Password)
-        try {
-            const response = await fetch("http://127.0.0.1:3000/api/blogs/create", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username: username,
-                    title: title,
-                    content: content
-                })
-            });
-            const data = await response.json();
-            if (response.status == 200 || response.status == 201) {
-                console.log(response, data);
-                handleClose();
-            } else {
-                console.log('Server responded with status:', response.status);                
-            }
-
-        } catch (e) {
-
-            console.error(e);
-        }
-    }
-
-    function handleSubmit() {
-        console.log(username);
-    }
+    } 
     return (
         <>
-            <Button color='inherit' onClick={goToWritePage}>Create Blog</Button>
-            
+            <Button color='inherit' onClick={goToWritePage}>Create Blog</Button>            
         </>
     );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,16 +9,51 @@ import CreateDialog from './CreateDialog';
 import CreateBlog from './CreateBlog';
 import RetrieveUserBlog from './RetrieveUserBlogs';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function NavBar({ authorize_flag, setAuthorize_flag }) {
+   //create setUser using context to pass to createBlog
+   const {setUser} = useAuth();
   const [open, setOpen] = useState(false);
   const [signupOpen, setsignupOpen] = useState(false);
   const navigate = useNavigate();
+
+  async function endpoint_call() {
+    //console.log(Username, Password)
+    try {
+      const response = await fetch("http://127.0.0.1:3000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "Guest",
+          password: "asdf1234"
+        })
+      });
+      const data = await response.json();
+      if (response.status == 200) {
+        console.log(response, data);        
+        setAuthorize_flag(true);    
+        //Pass on the username to make it globally accessible
+        setUser("Guest");  
+      }else{
+        setAuthorize_flag(false);
+      }
+
+    } catch (e) {
+
+      console.error(e);
+    }
+  }
 
   function handleLogin() {
     setOpen(true);
   }
 
+  function handleGuestLogin(){
+    endpoint_call();
+  }
   function handleClose() {
     setOpen(false);
     setsignupOpen(false);
@@ -38,7 +73,7 @@ export default function NavBar({ authorize_flag, setAuthorize_flag }) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, cursor:'pointer'}} onClick={handleBlogBtn}>
             Blogs
           </Typography>
-          {!authorize_flag && <Button color="inherit" onClick={handleLogin}>Guest Login</Button>}
+          {!authorize_flag && <Button color="inherit" onClick={handleGuestLogin}>Guest Login</Button>}
           {!authorize_flag && <Button color="inherit" onClick={handleLogin}>Login</Button>}
           {!authorize_flag && <Button color="inherit" onClick={handleSignup}>Sign Up</Button>}
           {authorize_flag && <CreateBlog />}
